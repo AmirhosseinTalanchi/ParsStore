484899310aa7f7b9cb0d346e55ee5ab4dcddb23a
AmirhosseinTalanchi <www.amirhosseintalanchi.84@gmail.com>
2026-01-29 15:37:33 +0330

    lunch project and create top bar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Home from './Pages/Home'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div dir='rtl' className='flex justify-center'>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { IoIosSearch } from "react-icons/io";
+import { CgUser } from "react-icons/cg";
+import { BsCart3 } from "react-icons/bs";
+import { FiMenu } from "react-icons/fi";
+
+
+
+export default function TopBar() {
+  return (
+    <div className='container flex justify-between items-center px-4 h-14 w-full'>
+
+      {/* logo page */}
+      <img src='public/Photo/logo.png' className='w-36 h-6'></img>
+
+
+      {/* search box */}
+      <div className='hidden lg:flex w-1/2 relative'>
+        <input type="text" placeholder='جستوجو در محصولات ...' className='w-full py-3 px-4 border border-zinc-300 rounded-xl outline-0' />
+        <IoIosSearch  className='w-6 h-6 absolute top-3  left-3'/>
+      </div>
+
+
+
+      {/* cart and account and menuInMoblie */}
+      <div className='flex gap-3 items-center'>
+        {/* accont */}
+        <div className='flex'>
+          <CgUser className='w-6 h-6' />
+          <span className='hidden lg:block text-zinc-500'>حساب کاربری</span>
+        </div>
+        {/* cart */}
+        <div className='flex'>
+          <div className='relative'>
+            <BsCart3 className='w-6 h-6' />
+            <div className='w-5 h-5 rounded-[7px] bg-blue-700 text-white justify-center items-center flex absolute bottom-3 right-3 text-sm'>0</div>
+          </div>
+          <span className='hidden lg:block text-zinc-500'>محصول</span>
+        </div>
+        {/* menuInMoblie */}
+        <FiMenu className='w-9 h-9 lg:hidden' />
+      </div>
+
+    </div>
+  )
+}
