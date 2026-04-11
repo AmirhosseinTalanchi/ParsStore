82370f5cb0db4b49f5bbc6c4344bb71dc0f07b3b
AmirhosseinTalanchi <www.amirhosseintalanchi.84@gmail.com>
2026-04-11 19:19:08 +0330

    responsive some part

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,30 +7,34 @@ export default function Footer() {
          <div className='flex flex-col justify-center items-center w-full bg-zinc-900 mt-15'>
             {/* footer top */}
             <div className='container'>
-               <div className=' flex justify-between px-10 py-7'>
-                  <div className='flex gap-10'>
-                     <div className='flex items-center gap-2'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white icon icon-tabler icons-tabler-outline icon-tabler-headset">
-                           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                           <path d="M4 14v-3a8 8 0 1 1 16 0v3"></path>
-                           <path d="M18 19c0 1.657 -2.686 3 -6 3"></path>
-                           <path d="M4 14a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2v-3z"></path>
-                           <path d="M15 14a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2v-3z"></path>
-                        </svg>
-                        <span className='text-white'>12345</span>
+               <div className=' flex justify-between  px-10 py-7 w-full'>
+                  <div className='flex flex-col md:flex-row gap-10 w-full md:w-50'>
+                     <div className='flex items-center justify-between md:justify-start gap-2 w-full'>
+                        <div className='flex items-center gap-2 '>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white icon icon-tabler icons-tabler-outline icon-tabler-headset">
+                              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                              <path d="M4 14v-3a8 8 0 1 1 16 0v3"></path>
+                              <path d="M18 19c0 1.657 -2.686 3 -6 3"></path>
+                              <path d="M4 14a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2v-3z"></path>
+                              <path d="M15 14a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2v-3z"></path>
+                           </svg>
+                           <span className='text-white'>12345</span>
+                        </div>
                         <label className='bg-zinc-800 text-zinc-500 text-xs rounded-lg px-1.5 py-1'>تلفن</label>
                      </div>
-                     <div className='flex items-center gap-2'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white icon icon-tabler icons-tabler-outline icon-tabler-mail">
-                           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                           <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
-                           <path d="M3 7l9 6l9 -6"></path>
-                        </svg>
-                        <span className='text-white'>support@example.com</span>
+                     <div className='flex items-center justify-between gap-2'>
+                        <div className='flex items-center justify-between gap-2'>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white icon icon-tabler icons-tabler-outline icon-tabler-mail">
+                              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                              <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
+                              <path d="M3 7l9 6l9 -6"></path>
+                           </svg>
+                           <span className='text-white'>support@example.com</span>
+                        </div>
                         <label className='bg-zinc-800 text-zinc-500 text-xs rounded-lg px-1.5 py-1'>ایمیل</label>
                      </div>
                   </div>
-                  <div className='flex'>
+                  <div className='hidden md:flex items-center gap-2'>
                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white icon icon-tabler icons-tabler-outline icon-tabler-clock-hour-8">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
@@ -43,20 +47,20 @@ export default function Footer() {
                </div>
             </div>
 
-            {/* footer button */}
          </div>
+         {/* footer button */}
 
          <div className='flex flex-col justify-center items-center w-full bg-zinc-800'>
             <div className='container'>
                <div className=' bg-zinc-800 flex justify-between flex-col pt-10'>
-                  <div className='flex justify-between text-zinc-400  bg-zinc-800 gap-7'>
+                  <div className='flex  text-zinc-400  bg-zinc-800 gap-7 flex-col xl:flex-row items-center justify-between'>
 
-                     <div className='flex gap-15'>
+                     <div className='flex flex-col lg:flex-row gap-15'>
                         <div className='flex flex-col gap-3'>
                            <img src='public/Photo/logo-dark.png' className='w-36 h-6'></img>
-                           <p className='w-lg'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطر آن چنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می‌باشد. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
+                           <p className='min-w-1'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطر آن چنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می‌باشد. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</p>
                         </div>
-                        <div className='flex gap-40'>
+                        <div className='flex gap-40 min-w-100'>
                            <ul className='flex flex-col gap-3'>
                               <li>خانه</li>
                               <li>تایپوگرافی</li>
--- src/components/SliderBanner.jsx
+++ src/components/SliderBanner.jsx
@@ -5,10 +5,10 @@ export default function SliderBanner({img,toptitle,title,caption}) {
   return (
     <div className='rounded-xl mb-10 relative'>
       <img className='rounded-xl' src={img} />
-      <p className='absolute right-20 top-20 text-xs text-[#FFFFFF7D]'>{toptitle}</p>
-      <h2 className='absolute right-20 top-26 text-[42px] text-white'>{title}</h2>
-      <p className='absolute right-20 top-44 text-[16px] text-[#FFFFFF9C]'>{caption}</p>
-      <button className='absolute right-20 top-56 text-[#1462CF] bg-[#FFFEFE] font-bold text-sm w-32 px-1 h-10 rounded-lg'>همین الان خرید کن</button>
+      <p className='hidden md:block absolute right-20 top-10 lg:top-14 xl:top-20 text-xs text-[#FFFFFF7D]'>{toptitle}</p>
+      <h2 className='absolute right-10 md:right-20 top-5 md:top-15 lg:top-20 xl:top-26  text-lg md:text-[30px] xl:text-[42px] text-white'>{title}</h2>
+      <p className='hidden lg:block absolute right-20 top-30 lg:top-35 xl:top-44 text-[13px] xl:text-[16px] text-[#FFFFFF9C]'>{caption}</p>
+      <button className='absolute right-10 md:right-20 top-15 md:top-30 lg:top-45 xl:top-56 text-[#1462CF] bg-[#FFFEFE] font-bold text-[10px] md:text-sm w-28 md:w-32 md:px-1 h-6 md:h-10 rounded-lg'>همین الان خرید کن</button>
     </div>
   )
 }
